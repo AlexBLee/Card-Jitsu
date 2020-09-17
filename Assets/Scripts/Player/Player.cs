a2adc8696a54f2bdb090257cf46b935f8412d2ff
Alex Lee <alexanderbevislee@gmail.com>
2020-09-17 01:00:37 -0700

    Can now send card info over to both clients

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -25,35 +25,49 @@ public class Player : MonoBehaviour
         deck = new Deck();
     }
 
-    public void PlayCardRPC(int index)
-    {
-        GetComponent<PhotonView>().RPC("PlayCard", RpcTarget.All, index);
-    }
-
     private void Update() {
         if (Input.GetKeyDown(KeyCode.V))
         {
-            Card xx = (Card)PhotonNetwork.CurrentRoom.CustomProperties["a"];
-            Debug.LogError(xx.GetCardStats());
+            Card xx = (Card)PhotonNetwork.CurrentRoom.CustomProperties["p1Card"];
+            Card xx2 = (Card)PhotonNetwork.CurrentRoom.CustomProperties["p2Card"];
+
+            Debug.LogError("Player 1: " + xx.GetCardStats());
+            Debug.LogError("Player 2: " + xx2.GetCardStats());
+
         }
     }
 
-    [PunRPC]
     public void PlayCard(int index)
     {
         cardPlayed = deck.cardList[index];
 
-        if (PhotonNetwork.CurrentRoom.CustomProperties["a"] != null)
+        if (PhotonNetwork.IsMasterClient)
         {
-            PhotonNetwork.CurrentRoom.CustomProperties["a"] = cardPlayed;
-            PhotonNetwork.CurrentRoom.SetCustomProperties(Launcher.customProperties);
+            if (PhotonNetwork.CurrentRoom.CustomProperties["p1Card"] != null)
+            {
+                PhotonNetwork.CurrentRoom.CustomProperties["p1Card"] = cardPlayed;
+                PhotonNetwork.CurrentRoom.SetCustomProperties(Launcher.customProperties);
+            }
+            else
+            {
+                Launcher.customProperties.Add("p1Card", cardPlayed);
+                PhotonNetwork.CurrentRoom.SetCustomProperties(Launcher.customProperties);
+            }
         }
         else
         {
-            Launcher.customProperties.Add("a", cardPlayed);
-            PhotonNetwork.CurrentRoom.SetCustomProperties(Launcher.customProperties);
-
+            if (PhotonNetwork.CurrentRoom.CustomProperties["p2Card"] != null)
+            {
+                PhotonNetwork.CurrentRoom.CustomProperties["p2Card"] = cardPlayed;
+                PhotonNetwork.CurrentRoom.SetCustomProperties(Launcher.customProperties);
+            }
+            else
+            {
+                Launcher.customProperties.Add("p2Card", cardPlayed);
+                PhotonNetwork.CurrentRoom.SetCustomProperties(Launcher.customProperties);
+            }
         }
+
 
 
 
